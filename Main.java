9cdba9cdfd5fbab08dd77bce9e121297782813f6
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2023-03-31 09:50:51 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,3 +10,19 @@ public class Main {
 		bill.Imprimir();
 	}
 }
+
+package jaiza;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Animal bil = new Animal();
+		
+		//chamando metodo para add nome
+		bil.AddNome("Bill Santos");
+		bil.AddNome(2.4);
+		bil.Imprimir();
+	}
+}
+
