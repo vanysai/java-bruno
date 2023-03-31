72a89f82b50c8390ea88881ec24b86086825204b
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2023-03-31 09:18:39 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package jaiza;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Animal bill = new Animal();
+		bill.nome = "Bill Souza";
+		bill.peso = 2.5;
+		bill.Imprimir();
+	}
+}
