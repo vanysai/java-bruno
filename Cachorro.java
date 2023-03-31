97cfbe49080331ecb6c0e216e736f3007e0e8295
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2023-03-31 09:50:25 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,23 @@
+package jaiza;
+
+public class Cachorro {
+	//atributo
+	public String nome;
+	
+	//construtor
+	public Cachorro(String nome) {
+		this.nome = nome;
+	}
+	
+	//metodos
+	//getters and setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
