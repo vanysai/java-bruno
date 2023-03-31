e7359416ed72e6112c15d2a78d39358d80105d0f
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2023-03-31 09:51:22 -0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -13,3 +13,40 @@ public class Animal {
 					"\nPeso: "+ this.peso);
 		}
 }
+
+
+package jaiza;
+
+public class Animal {
+	
+		//encapsulamento
+		private String nome;
+		private double peso;
+		
+		//construtores
+		public Animal() {}
+		
+		
+		public Animal(String nome, double peso) {
+		}
+		public Animal(String nome){
+		}
+		
+		//métodos
+		public void Imprimir() {
+			System.out.println("Nome: " + this.nome + "\nPeso: " + this.peso);
+		
+		}
+		//criar metodo para atribuir e recuperar o valor
+		//da variavel
+		
+		public void AddNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		public void AddNome(double peso) {
+			this.peso = peso;
+		}
+		
+}
